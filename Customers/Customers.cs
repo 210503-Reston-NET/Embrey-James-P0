73df0ec2a5ec313a5b704ad9b917648ef983db76
jpatemb <jpat.embrey@gmail.com>
2021-05-12 22:45:38 -0400

    Added more files and namespace. Expanded on files such as Products, etc.

---
+++ Customers/Customers.cs
@@ -0,0 +1,18 @@
+using System;
+
+
+namespace Customers
+{
+    class AddCustomer
+    {
+        /// <summary>
+        /// Allows addition of new customers
+        /// </summary>
+        /// <param name="args"></param>
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This page is used for adding a customer!");
+        }
+    }
+}
---
+++ Location/Location.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Locations
+{
+
+    class Program
+    {
+        /// <summary>
+        /// Places orders to store locations
+        /// </summary>
+        /// <param name="args"></param>
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your store location here:");
+            Console.WriteLine("This is a store location!");
+        }
+    }
+}
